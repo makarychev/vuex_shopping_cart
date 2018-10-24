741ed98f49821ad8c7c084f62cc598b41e64914e
Maksym Makarychev <Maksym.Makarychev@ingenico.com>
2018-10-24 12:42:38 +0300

    add: implement add to cart button

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        product: []
+        product: [],
+        // {id, quantity}
+        cart: []
     },
 
     getters: { // = computed properties
@@ -29,19 +31,39 @@ export default new Vuex.Store({
             
         },
 
-        // addToCard (context, product) {
-        //     if (product.inventory > 0) {
-        //         context.commit('pushProductToCard', product)
-        //     } else {
-        //         // show out of stock message
-        //     }
-        // }
+        addProductToCard (context, product) {
+            if (product.inventory > 0) {
+                const cartItem = context.state.cart.find(item => item.id === product.id)
+                if (!cartItem) {
+                    context.commit('pushProductToCart', product.id)
+                } else {
+                    context.commit('incrementItemQuantity', cartItem)
+                }
+                context.commit('decrementProductInventory', product)
+            }
+        }
     },
 
     mutations: {
         setProducts (state, products) {
             // update products
             state.products = products
+        },
+
+        // const cartItem = {id: 123, quantity: 2}
+        pushProductToCard (state, productId) {
+            state.cart.push({
+                id: productId,
+                quantity: 1
+            })
+        },
+
+        incrementItemQuantity (state, cartItem) {
+            cartItem.quantity++
+        },
+
+        decrementProductInventory (state, product) {
+            product.inventory--
         }
     }
 })
