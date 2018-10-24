207a113c0f0132c5bc68e8a391963470f45a2e2d
Maksym Makarychev <Maksym.Makarychev@ingenico.com>
2018-10-24 11:33:02 +0300

    add: get product list from api func

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        product: []
+    },
+
+    getters: {
+        productsCount () {
+            /// ...
+        }
+    },
+
+    actions: {
+        fetchProducts (){
+            // make the call
+            // run setProducts mutation
+        }
+    },
+
+    mutations: {
+        setProducts (state, products) {
+            // update products
+            state.products = products
+        }
+    }
+})
