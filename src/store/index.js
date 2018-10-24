0e5a24b04f0e43a260cd18541181a91057ef51c9
Maksym Makarychev <Maksym.Makarychev@ingenico.com>
2018-10-24 15:22:13 +0300

    add: total price of product calc

--- src/store/index.js
+++ src/store/index.js
@@ -15,6 +15,26 @@ export default new Vuex.Store({
     getters: { // = computed properties
         avaliableProducts (state, getters) {
             return state.product.filter(product => product.inventory > 0)
+        },
+
+        cartProducts (state) {
+            return state.cart.map(cartItem => {
+                const product = state.products.find(product => product.id === cartItem.id)
+                return {
+                    title: product.title,
+                    price: product.price,
+                    quantity: cartItem.quantity
+                }
+            })
+        },
+
+        cartTotal (state, getters) {
+            // let total = 0
+            // getters.cartProducts.forEach(product => {
+            //     total += product.price * product.quantity
+            // })
+            // return total
+            return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
         }
     },
 
